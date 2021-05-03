c764226c957e074a71813c655fb40028d086e2a2
Mhieu181120 <mhieu181120@gmail.com>
2021-05-03 13:18:42 +0700

    Update Add.jsx

--- code/fe/src/components/common/Add.jsx
+++ code/fe/src/components/common/Add.jsx
@@ -10,11 +10,10 @@ import {getCurrentDate} from './utils'
 
 export default function Add() {
 
-    const [ttdieukien,setttDieukien]=useState(false)
-    const [dieukien,setDieukien]=useState('')
+    
+    const [dieukien,setDieukien]=useState([{check:false,input:''}])
 
-    const [ttdieukien_1,setttDieukien_1]=useState(false)
-    const [dieukien_1,setDieukien_1]=useState('')
+    
 
     const [form] = Form.useForm();
 
@@ -28,7 +27,6 @@ export default function Add() {
         });
         
     };
-<<<<<<< HEAD
 
     const [textTriGIa,setTextTriGia]=useState(0);
     const onChangeTriGia =(e)=>{
@@ -54,18 +52,12 @@ export default function Add() {
       
 
     
-=======
->>>>>>> ea2543355a798b04dd5bc0de66511e278662e58c
     const [image,setImage]=useState('/img/manager-1.jpg');
     const onImageChange = (e) =>{
         if(e.target.files && e.target.files[0]){
             let img = e.target.files[0];
             setImage(URL.createObjectURL(img))
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> ea2543355a798b04dd5bc0de66511e278662e58c
         }
     }
 
